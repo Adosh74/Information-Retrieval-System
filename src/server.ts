d714681f03440210a365022a627dd3b15100d36d
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-11-20 23:37:52 +0200

    use tokenizer and stemmer functions

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
+import fs from 'fs';
 import { app } from './app';
+import Stemming from './utils/stemming';
+import Tokenization from './utils/tokenization';
+
+const collection = fs
+	.readdirSync('./collection')
+	.sort((a, b) => parseInt(a) - parseInt(b));
+
+console.log(collection);
+
+Tokenization('fools fear in rush to tread where').forEach((token) => {
+	console.log(Stemming(token));
+});
 
 app.listen(3001, () => {
 	console.log('Server started on port 3001');
