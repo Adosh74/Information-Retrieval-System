6f78bba35ea75ca0bd97c34623340f74b520fab3
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-01 13:31:19 +0200

    use all new features

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,39 @@
 import fs from 'fs';
 import { app } from './app';
+import createPositionalIndex from './utils/newPositionalIndex';
+import searchPhrase from './utils/searchPhrase';
 import Stemming from './utils/stemming';
 import Tokenization from './utils/tokenization';
 
-//
+// eslint-disable-next-line prefer-const
+let fileTerms: string[][] = [[]];
 // read collection
 fs.readdirSync('./collection')
 	.sort((a, b) => parseInt(a) - parseInt(b))
 	.forEach((fileName) => {
-		console.log(fileName);
 		const fileContent: string = fs.readFileSync(
 			`${process.cwd()}/collection/${fileName}`,
 			'utf-8'
 		);
-
+		// eslint-disable-next-line prefer-const
+		let fileTermsArr: string[] = [];
+		// tokenization and stemming
 		Tokenization(fileContent).forEach((token) => {
-			console.log(Stemming(token));
+			fileTermsArr.push(Stemming(token));
 		});
+		fileTerms.push(fileTermsArr);
 	});
+
+fileTerms.shift();
+// console.log(fileTerms);
+
+// create query and search it
+const query = 'angels fools in rush';
+const normalizedQuery = Tokenization(query.toLowerCase()).map((token) => {
+	return Stemming(token);
+});
+
+console.log(searchPhrase(createPositionalIndex(fileTerms), normalizedQuery));
 
 // test a query with tokenization and stemming
 
