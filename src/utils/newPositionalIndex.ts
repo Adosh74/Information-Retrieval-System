8946e9c548543df377f9d09521a968ab2a710772
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-01 13:29:16 +0200

    implement new positional index

---
+++ src/utils/newPositionalIndex.ts
@@ -0,0 +1,45 @@
+export type PositionalIndex = {
+	[term: string]: {
+		docCount: number;
+		positions: {
+			[docId: string]: number[]; // Change here
+		};
+	};
+};
+
+const createPositionalIndex = (documents: string[][]): PositionalIndex => {
+	const positionalIndex: PositionalIndex = {};
+
+	// Iterate over each document
+	for (let docId = 0; docId < documents.length; docId++) {
+		const document = documents[docId];
+
+		// Iterate over each term in the document
+		for (let position = 0; position < document.length; position++) {
+			const term = document[position];
+
+			// If the term doesn't exist in the positional index, initialize it
+			if (!(term in positionalIndex)) {
+				positionalIndex[term] = {
+					docCount: 0,
+					positions: {},
+				};
+			}
+
+			const termInfo = positionalIndex[term];
+
+			// If the document doesn't exist in the term's positions, initialize it
+			if (!(docId.toString() in termInfo.positions)) {
+				termInfo.positions[docId.toString()] = [];
+				termInfo.docCount++;
+			}
+
+			// Add the position of the term in the document
+			termInfo.positions[docId.toString()].push(position);
+		}
+	}
+
+	return positionalIndex;
+};
+
+export default createPositionalIndex;
