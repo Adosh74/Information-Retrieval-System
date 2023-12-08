a86dd7c9d07972d2ba302e90393cdcb373b88dd1
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-08 14:35:10 +0200

    adding route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express';
+import routes from './routes/index.route';
 
 export const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.get('/healthz', (req: Request, res: Response) => {
 	res.status(200).json({
