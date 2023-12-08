5ce7882b674c65817caa2d4870c8c30a3a8f103a
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-08 14:33:52 +0200

    create search route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { phraseQuery } from './../controllers/query.controller';
 
 const router = Router();
+
+router.get('/search', phraseQuery);
 
 export default router;
